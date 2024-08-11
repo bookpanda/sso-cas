0178abc4670a4ff8d8fcc0efa42afdf4ce5ccfb9
Idhibhat Pankam <marginpankam@gmail.com>
2024-08-11 14:57:40 +0700

    markup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { FcGoogle } from "react-icons/fc";
+
 function App() {
-  return <>s</>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+      <div className="flex h-[40vh] w-[60vw] flex-col items-center rounded-xl bg-white px-8 py-[10vh] drop-shadow-xl md:w-[40vw] xl:w-[30vw] 2xl:w-[20vw]">
+        <h1 className="text-4xl font-bold">SSO Login</h1>
+        <button className="mt-8 flex items-center justify-center rounded-lg border border-gray-300 px-[30%] py-2 text-lg text-gray-600 duration-300 ease-in-out hover:bg-slate-100">
+          <FcGoogle className="mr-2 inline h-8 w-8" /> <h3>Google</h3>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
