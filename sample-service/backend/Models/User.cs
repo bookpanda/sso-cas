f0dddeb81c1215c1a4a7f8f2f1b3a8571bd4360c
Idhibhat Pankam <marginpankam@gmail.com>
2024-08-12 15:34:32 +0700

    user id gen

--- sample-service/backend/Models/User.cs
+++ sample-service/backend/Models/User.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace backend.Models;
 
 public class User
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string? ID { get; set; }
     public required string CASID { get; set; }
 }
